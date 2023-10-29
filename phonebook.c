5c2f69106bceaa28fcd1af60040811a943c5fe5e
Fidel Otieno <fidelotieno11@gmail.com>
2023-10-30 01:44:31 +0300

    Phone book searching modified with our own datatype

--- phonebook.c
+++ phonebook.c
@@ -2,17 +2,31 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct      
+{
+    string name;
+    string number;
+}
+person;
+
+
 int main (void)
 {
-    string people[] = {"Fidel", "Ryan", "Leon"};
-    // array of phone numbers in string format and country code
-    string number[] = {"+461-264-462", "+254-112-741449", "+294-3453-3477"};
+    person people[3];
+
+    people[0].name = "John"; 
+    people[0].number = "617-324-9876"; 
+    people[1].name = "Bob";
+    people[1].number = "555-555-5555";
+    people[2].name = "Alice";
+    people[2].number = "867-5309";
+
     string name = get_string("Name: ");
     for(int i=0; i<3; i++)
     {
-        if (strcmp(people[i],name) == 0)
+        if (strcmp(people[i].name,name) == 0)
         {
-            printf("Name: %s\nNumber: %s\n\n", people[i], number[i]);
+            printf("Name: %s\nNumber: %s\n\n", people[i].name, people[i].number);
             return 0;
         }
         
