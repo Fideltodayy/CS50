106982763c5ac254ec9c752eb276e9ee6f3cfebb
Fidel Otieno <fidelotieno11@gmail.com>
2023-10-30 13:44:31 +0300

    Discovered that a string is a pointer to a character and got rid of the string datatype from cs50.h

---
+++ strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// #include "cs50.h"
+
+int main(void)
+{
+    char* s = "Hi!";
+
+    // prints: Hi!
+    printf("%s\n", s); 
+    // printf("%p\n", &s[0]);
+    // printf("%p\n", &s[1]);
+    // printf("%p\n", &s[2]);
+    // printf("%p\n", &s[3]);
+}
