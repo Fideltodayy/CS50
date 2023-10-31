27e03cf6db8cc819214b2142cc321d84ded073ef
Fidel Otieno <fidelotieno11@gmail.com>
2023-10-31 12:04:11 +0300

    Get Int without cs50 header file

---
+++ getInt.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int x;
+    printf("Enter a number: ");
+    scanf("%i", &x);
+    printf("X is %i",x);
+}
---
+++ swap.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap (int *a, int *b);
+
+int main (void)
+{
+    int x = 1;
+    int y = 2;
+
+    printf("X is %i and Y is %i\n", x , y);
+    swap(&x,&y);
+    printf("X is %i and Y is %i\n", x , y);
+
+}
+
+void swap (int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+    printf("X is %i and Y is %i\n", *a , *b);
+
+}
